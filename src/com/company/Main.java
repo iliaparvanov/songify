f2db3680ff0a7041d9d6321403adf5cba4ed3faf
Shimo <simeonn.h.georgiev@gmail.com>
2019-05-28 20:17:53 +0300

    added create ui

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,23 +1,70 @@
 package com.company;
 
 import com.company.controllers.AlbumsController;
+import com.company.controllers.ArtistsController;
+import com.company.controllers.GenresController;
 import com.company.controllers.SongsController;
 import com.company.db_builder.TableInitializer;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) throws SQLException {
-            TableInitializer.createGenreTable();
-            TableInitializer.createSongTable();
-            TableInitializer.createAlbumTable();
+        TableInitializer.createGenreTable();
+        TableInitializer.createSongTable();
+        TableInitializer.createAlbumTable();
+        TableInitializer.createArtistTable();
 
-        Song song = new Song("asf", "2008-10-10", "2:53", AlbumsController.find(1));
-        SongsController.create(song);
-        SongsController.update(4, "asdf", "2012-12-12", "3:00", "6");
-        SongsController.index();
+
+
+        System.out.println("Wellcome to Songify");
+
+        Scanner scanner = new Scanner(System.in);
+        String crud = scanner.next();
+        String type = scanner.next();
+        switch (crud){
+            case "find":
+                System.out.println("Enter the song name");
+
+                break;
+            case "create":
+                switch (type){
+                    case "song":
+
+                        Song song = new Song(scanner.next(),scanner.next(), scanner.next(), null);
+                        SongsController.create(song);
+                        break;
+
+                    case "genre":
+                        Genre genre = new Genre(scanner.next());
+                        GenresController.create(genre);
+                        break;
+
+                    case "artist":
+                        Artist artist = new Artist(scanner.next());
+                        ArtistsController.create(artist);
+                        break;
+
+                    case "album":
+                        Album album = new Album(scanner.next(), 1);
+                        AlbumsController.create(album);
+                        break;
+
+                }
+
+                break;
+
+            case "update":
+                System.out.println("");
+
+            case "delete":
+
+            case "index":
+        }
+
 
     }
 }
