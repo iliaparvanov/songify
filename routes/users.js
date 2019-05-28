f5500cab5b6c33e56325c8a8460b8385cd27f8ab
Gdemirev01 <gdemirev78@gmail.com>
2019-05-28 14:18:44 +0300

    asdf

--- routes/users.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/login', (req, res) => res.send('Login page'));
-router.get('/register', (req, res) => res.send('Register page'));
-
-module.exports = router;
